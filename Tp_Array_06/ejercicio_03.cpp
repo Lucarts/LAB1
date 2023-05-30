0760f6230595a052fb95a1fa6fefafa1b51c9835
Lucarts <lucarts81@gmail.com>
2023-05-29 22:59:18 -0300

    Ejercicio 03, resuelto (puede que haya que hacerle alguna correccion)

--- Tp_Array_06/ejercicio_03.cpp
+++ Tp_Array_06/ejercicio_03.cpp
@@ -12,5 +12,40 @@ using namespace std;
 
 int main()
 {
+  const int ELEMENTOS = 15;
+  int vector[ELEMENTOS] = {};
+  int menor;
+  int contadorMinimo = 0;
+
+  cout << "Ingrese " << ELEMENTOS << " numeros enteros. " << endl;
+
+  for (int i = 0; i < ELEMENTOS; i++)
+  {
+    cout << "Entero nro " << i + 1 << ": ";
+    cin >> vector[i];
+  }
+
+  menor = vector[0];
+
+  for (int i = 1; i < ELEMENTOS; i++)
+  {
+    if (vector[i] < menor)
+    {
+      menor = vector[i];
+      contadorMinimo++;
+    }
+    else
+    {
+      if (menor == vector[i])
+      {
+        contadorMinimo++;
+      }
+    }
+  }
+
+  // Tener en cuenta que la solucion puede estar no realizada correctamente.
+  cout << endl;
+  cout << "El numero con valor minimo ingresado es " << menor << " y se repite " << contadorMinimo << " veces." << endl;
+
   return 0;
 }
