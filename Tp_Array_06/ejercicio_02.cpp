7ab4dcfb3ad4e893507b2bfd908524255b7e2da5
Lucarts <lucarts81@gmail.com>
2023-05-29 22:35:20 -0300

    Ejercicio 02, resuelto

--- Tp_Array_06/ejercicio_02.cpp
+++ Tp_Array_06/ejercicio_02.cpp
@@ -12,5 +12,39 @@ using namespace std;
 
 int main()
 {
+  const int CANTIDAD = 15;
+  int numeros[CANTIDAD] = {};
+  bool coincidencia = false;
+  int num;
+  string mensaje;
+
+  cout << "Ingrese " << CANTIDAD << " numeros por favor: " << endl;
+  for (int i = 0; i < CANTIDAD; i++)
+  {
+    cout << "Numero # " << i + 1 << ": ";
+    cin >> numeros[i];
+  }
+
+  cout << endl;
+  cout << "Ingrese un numero: \t ";
+  cin >> num;
+
+  for (int i = 0; i < CANTIDAD; i++)
+  {
+    if (num == numeros[i])
+    {
+      coincidencia = true;
+    }
+  }
+
+  cout << "--------------------------------------" << endl;
+  if (coincidencia == true)
+  {
+    cout << "El numero que ingresaste esta en la lista de numeros ingresados" << endl;
+  }
+  else
+  {
+    cout << "El numero ingresado NO esta en la lista " << endl;
+  }
   return 0;
 }
