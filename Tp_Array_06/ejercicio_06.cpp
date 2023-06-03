55be3878a8bb2b3e7398ef187f24dd0d8341353b
Lucarts <lucarts81@gmail.com>
2023-06-02 23:10:11 -0300

    Tp 6, ejercicio 06 realizado correctamente - Macbook

--- Tp_Array_06/ejercicio_06.cpp
+++ Tp_Array_06/ejercicio_06.cpp
@@ -15,5 +15,46 @@ using namespace std;
 
 int main()
 {
+  const int ELEMENTOS = 10;
+  int vector[ELEMENTOS] = {};
+  int maximo, indice;
+
+  cout << endl;
+  cout << "Ingrese " << ELEMENTOS << " numeros. " << endl;
+
+  for (int i = 0; i < ELEMENTOS; i++)
+  {
+    cout << "# " << i + 1 << ": ";
+    cin >> vector[i];
+  }
+
+  maximo = vector[0];
+  indice = 0;
+
+  for (int i = 1; i < ELEMENTOS; i++)
+  {
+    if (vector[i] > maximo)
+    {
+      maximo = vector[i];
+      indice = i;
+    }
+  }
+  cout << endl;
+  cout << "--------------------------" << endl;
+  cout << " Maximo ---------> " << maximo << " y su posicion indice es " << indice << endl;
+
+  for (int i = indice + 1; i < ELEMENTOS; i++)
+  {
+    vector[i - 1] = vector[i];
+  }
+
+  cout << endl;
+  cout << "{ ";
+  for (int i = 0; i < ELEMENTOS - 1; i++)
+  {
+    cout << vector[i] << " ";
+  }
+  cout << "}" << endl;
+
   return 0;
 }
