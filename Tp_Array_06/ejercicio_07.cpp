431fbe23b9178548d3c8cc9567d65e7ab5b0e131
Lucarts <lucarts81@gmail.com>
2023-06-03 00:02:59 -0300

    tp 6 ejercicio 07 falta hacer unos ajustes y corregir, no esta mal del todo - Macbook

--- Tp_Array_06/ejercicio_07.cpp
+++ Tp_Array_06/ejercicio_07.cpp
@@ -17,5 +17,42 @@ using namespace std;
 
 int main()
 {
+  const int LISTA = 10;
+  int vector[LISTA] = {};
+  int numero;
+
+  cout << endl;
+  cout << "Ingresar " << LISTA - 1 << " numeros" << endl;
+  cout << "Los numeros ingresados deben ser distintos entre si y ordenados de menor a mayor." << endl;
+
+  for (int i = 0; i < LISTA - 1; i++)
+  {
+    cout << "# " << i + 1 << ": ";
+    cin >> vector[i];
+  }
+  cout << endl;
+  cout << "Ahora ingresa otro numero, debe ser mayor al menor y menor al mayor " << endl;
+  cin >> numero;
+
+  // NO ESTA DEL TODO MAL
+  // Corregir ejercicio
+  for (int i = 0; i < LISTA; i++)
+  {
+    if (numero > vector[i] && numero < vector[i + 1])
+    {
+      vector[i] = numero;
+    }
+  }
+
+  cout << endl;
+  cout << "--------------" << endl;
+  cout << "{ ";
+  for (int i = 0; i < LISTA; i++)
+  {
+    cout << vector[i] << " ";
+  }
+  cout << "}";
+
+  cout << endl;
   return 0;
 }
